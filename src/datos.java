9f120c6b3245d3f4a5030744597c07600fe73f1c
DennisCatana <dennis.joel235@outlook.es>
2023-08-04 12:45:11 -0500

    Prueba2

---
+++ src/datos.java
@@ -0,0 +1,229 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class datos {
+    private JTextField codigo;
+    private JTextField cedula;
+    private JTextField nombre;
+    private JTextField fecha;
+    private JButton borrarButton;
+    private JButton actualizarButton;
+    private JButton ingresarButton;
+    private JButton limpiarFormularioButton;
+    private JButton buscarPorCodigoButton;
+    private JButton bucarPorNombreButton;
+    private JButton buscarPorSignoButton;
+    private JComboBox sig;
+    private JPanel datos; //Ojo aqui
+
+    //Conectar a la base
+    static final String DB_URL="jdbc:mysql://localhost/prueba";
+    static final String USER="root";
+    static final String PASS="root_bas3";
+    static final String QUERY="Select * From datos ;";
+    private boolean data = false;
+
+    //Datos que se necesitan para la comprobar
+    public static String nomx;
+    public static String fechx;
+    public static String sigx;
+
+    datosX persona = new datosX();
+    public datos(){
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conectar();
+                int codx = Integer.parseInt(codigo.getText());
+                eliminar(codx);
+            }
+        });
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conectar();
+                int codx = Integer.parseInt(codigo.getText());
+                int cedx = Integer.parseInt(cedula.getText());
+                nomx =nombre.getText().trim();
+                fechx = fecha.getText().trim();
+                sigx = String.valueOf(sig.getNextFocusableComponent());
+                ingresar(codx,cedx,nomx,fechx,sigx);
+            }
+        });
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        limpiarFormularioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                codigo.setText("");
+                cedula.setText("");
+                nombre.setText("");
+                fecha.setText("");
+                sig.setSelectedItem("");
+            }
+        });
+        buscarPorCodigoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setCodigo(codigo.getText());
+                try(
+                        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(QUERY);
+                ){
+                    while(rc.next()) {
+                        if (persona.getCodigo().equals(rc.getString("codigo"))) {
+                            nombre.setText(rc.getString("Nombre"));
+                            cedula.setText(rc.getString("cedula"));
+                            fecha.setText(rc.getString("FechaN"));
+                            sig.setSelectedItem(rc.getString("Signo"));
+                            data = false;
+                            break;
+                        }
+                        else{
+                            data = true;
+                        }
+                    }
+                    if(data == true){
+                        JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        bucarPorNombreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setNombre(nombre.getText());
+                try(
+                        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(QUERY);
+                ){
+                    while(rc.next()) {
+                        if (persona.getNombre().equals(rc.getString("Nombre"))) {
+                            codigo.setText(rc.getString("codigo"));
+                            cedula.setText(rc.getString("cedula"));
+                            fecha.setText(rc.getString("FechaN"));
+                            sig.setSelectedItem(rc.getString("Signo"));
+                            data = false;
+                            break;
+                        }
+                        else{
+                            data = true;
+                        }
+                    }
+                    if(data == true){
+                        JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        buscarPorSignoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                persona.setSigno((String) sig.getSelectedItem());
+                try(
+                        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+                        Statement stat = conn.createStatement();
+                        ResultSet rc = stat.executeQuery(QUERY);
+                ){
+                    while(rc.next()) {
+                        if (persona.getSigno().equals(rc.getString("Signo"))) {
+                            codigo.setText(rc.getString("codigo"));
+                            cedula.setText(rc.getString("cedula"));
+                            fecha.setText(rc.getString("FechaN"));
+                            nombre.setText(rc.getString("Nombre"));
+                            data = false;
+                            break;
+                        }
+                        else{
+                            data = true;
+                        }
+                    }
+                    if(data == true){
+                        JOptionPane.showMessageDialog(null,"USUARIO NO ENCONTRADO","Regitros",JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+
+    public static void eliminar(int cod){
+        String query2 = "DELETE FROM datos where codigo = '"+ cod +"'";
+        try(
+                Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); //Esencial para la conección
+                Statement stmt= conn.createStatement();
+        ){
+            stmt.executeUpdate(query2);
+            System.out.println("----------------------------------------------");
+            System.out.println("Usuario Eliminado");
+            System.out.println("----------------------------------------------");
+        }catch (Exception el){
+            throw new RuntimeException(el);
+        }
+    }
+    public static void ingresar(int cod, int ced, String nom, String fech,String signo){
+        String query3 = " insert into datos values('"+cod+"',+'"+ced+"',+'"+nom+"','"+fech+"','"+signo+"')";
+        try(
+                Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); //Esencial para la conección
+                Statement stmt= conn.createStatement();
+        ){
+            stmt.executeUpdate(query3);
+            System.out.println("----------------------------------------------");
+            System.out.println("Usuario Ingresado");
+            System.out.println("----------------------------------------------");
+        }catch (Exception el){
+            throw new RuntimeException(el);
+        }
+    }
+
+    public static void actualizar(String clave){
+        String query2 = "UPDATE datos set Clave = '"+1234+"'"+ "where Nombre="+'"'+ "Dennis" +'"' ;
+        //System.out.println(query2);
+        try(
+                Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); //Esencial para la conección
+                Statement stmt= conn.createStatement();
+        ){
+            stmt.executeUpdate(query2);
+            System.out.println("Usuario Actualizado xd");
+            System.out.println("----------------------------------------------");
+        }catch (Exception el){
+            throw new RuntimeException(el);
+        }
+    }
+    public static void conectar(){
+        try{
+            final String DB_URL="jdbc:mysql://localhost/prueba";
+            final String USER="root";
+            final String PASS="root_bas3";
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); //Esencial para la conección
+            Statement stmt= conn.createStatement();
+
+        }catch(SQLException S){
+            JOptionPane.showMessageDialog(null,S.getMessage(),"ERROR DE CONEXION",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("datos");
+        frame.setContentPane(new datos().datos);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/datosX.java
@@ -0,0 +1,60 @@
+public class datosX {
+    private String codigo;
+    private String cedula;
+    private String nombre;
+
+    private String fecha;
+    private String signo;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+
+    public datosX(String codigo, String cedula, String nombre, String fecha, String signo) {
+        this.codigo = codigo;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.signo = signo;
+    }
+
+    public datosX() {
+    }
+}
+
